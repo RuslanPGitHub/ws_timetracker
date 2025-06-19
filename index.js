81e90af50fac504ebe625c6a7d5052b4bc28eedf
Ruslan Popadiuk <r.popadiuk@avivi.com.ua>
2025-06-19 13:06:41 +0300

    updated views, packeges

--- index.js
+++ index.js
@@ -17,7 +17,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(router);
 
-
 app.use((req, res, next) => {
     res.status(404).render('404', { title: 'Page Not Found' });
 });
